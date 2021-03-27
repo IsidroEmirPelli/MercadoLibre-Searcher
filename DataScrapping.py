2d8d63c9b63a56d3dc6a7c515c5cad0279c6ce3d
Ostiz, Juan Manuel <w4mp1z@outlook.com>
2021-03-27 08:58:32 -0300

    Analiza todas las páginas posibles y búsqueda personalizada

--- DataScrapping.py
+++ DataScrapping.py
@@ -15,36 +15,50 @@ listaproductos = soup.find_all('div',{"class":"ui-search-result__content-wrapper
 #print(listaproductos) #DEBUGGING
 
 links_productos = []
-for producto in listaproductos:
-    link = producto.find("a",{"class":"ui-search-link"}).get('href') #El primer atributo "a" crea un enlace a archivos o una URL. Utilizo ".get()" para obtener el atributo "href" que es una referencia a una URL.
-    links_productos.append(link)
-#print(links_productos)
-b=' Cargando'
-with open ("info.txt","w") as file:
+sig = True
+with open(r'C:\Users\W4MP1\Desktop\info.txt', 'w') as file:
     file.write('Título' + '\t' + 'Precio' +'\t'+ 'Link\n')
-    for link in links_productos:
-        r = requests.get(link, headers=headers).text
-        hun = BeautifulSoup(r, 'html.parser')
+    while(sig):
+        links_productos.clear()
+        print('Lista desde 0')
+        for producto in listaproductos:
+            #print(len(listaproductos))
+            link = producto.find("a",{"class":"ui-search-link"}).get('href') #El primer atributo "a" crea un enlace a archivos o una URL. Utilizo ".get()" para obtener el atributo "href" que es una referencia a una URL.
+            links_productos.append(link)
+        b=' Cargando'
+        for link in links_productos:
+            r = requests.get(link, headers=headers).text
+            hun = BeautifulSoup(r, 'html.parser')
+            try:
+                Titulo = hun.find('h1', {'class':'ui-pdp-title'}).text.replace('\n','')
+            except:
+                Titulo = None
+            #print(Titulo)
+            try:
+                Precio = hun.find('span',{'class':'price-tag-fraction'}).text
+            except:
+                Precio = None
+            #print(Precio)
+            try:
+                Signo = hun.find('span',{'class':'price-tag-symbol'}).text
+            except:
+                Signo = None
+            #print(Signo)   
+            file.write(Titulo +'\t'+ Signo + Precio +'\t'+ link + '\n')
+            for i in range(1,4):
+                sys.stdout.write(b + "." * i +'\r')
+                sys.stdout.flush()
+                time.sleep(0.1)
+                sys.stdout.write('\033[2K\033[1G')
+        print('Termine archivo')
         try:
-            Titulo = hun.find('h1', {'class':'ui-pdp-title'}).text.replace('\n','')
+            link_sig = soup.find('div',{'class':'ui-search-pagination'})
+            link_sig = link_sig.find('li',{'class':'andes-pagination__button andes-pagination__button--next'}) #Buscamos la "li" que contenga el sig.
+            link_sig = link_sig.find('a', {'class':'andes-pagination__link ui-search-link'}).get('href')  #Obtenemos la URL de la siguente pagina.
+            r = requests.get(link_sig).text
+            soup=BeautifulSoup(r,'html.parser')
+            listaproductos = soup.find_all('div',{"class":"ui-search-result__content-wrapper"})
+            print('Pág. siguiente')
         except:
-            Titulo = None
-        #print(Titulo)
-        try:
-            Precio = hun.find('span',{'class':'price-tag-fraction'}).text
-        except:
-            Precio = None
-        #print(Precio)
-        try:
-            Signo = hun.find('span',{'class':'price-tag-symbol'}).text
-        except:
-            Signo = None
-        #print(Signo)
-        file.write(Titulo +'\t'+ Signo + Precio +'\t'+ link + '\n')
-        for i in range(1,4):
-            sys.stdout.write(b + "." * i +'\r')
-            sys.stdout.flush()
-            time.sleep(0.1)
-        sys.stdout.write('\033[2K\033[1G')
+            sig = False
 print('\nTerminé.')
-        
